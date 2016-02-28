0c00973aa1648267dd050862fe9f3767021c9a6b
Xiaoxiong Xing <xingxiaoxiong@frontier.hokudai.ac.jp>
2016-02-28 16:45:39 +0800

    persist room's width and length

--- LoveMyHome/HomeViewController.swift
+++ LoveMyHome/HomeViewController.swift
@@ -23,6 +23,14 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var zoomInButton: UIButton!
     @IBOutlet weak var zoomOutButton: UIButton!
+    @IBOutlet weak var roomWidthTextField: UITextField!
+    @IBOutlet weak var roomLengthTextField: UITextField!
+    
+    let RoomWidthKey = "Room Width Key"
+    let RoomLengthKey = "Room Length Key"
+    
+    var roomWidth: Float = 0.0
+    var roomLength: Float = 0.0
     
     var scene: SCNScene = SCNScene()
     var camera: SCNNode = SCNNode()
@@ -30,6 +38,12 @@ class HomeViewController: UIViewController {
     var geometryNode: SCNNode = SCNNode()
     var staticGeometry: SCNNode = SCNNode()
     var dynamicGeometry: SCNNode = SCNNode()
+    
+    // Room structure
+    var leftWallNode: SCNNode = SCNNode()
+    var rightWallNode: SCNNode = SCNNode()
+    var frontNode: SCNNode = SCNNode()
+    var backNode: SCNNode = SCNNode()
     var floorNode: SCNNode = SCNNode()
     
     var state: State = .Normal
@@ -51,6 +65,22 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         
         myDesign = NSKeyedUnarchiver.unarchiveObjectWithFile(designFilePath) as? [NodeInfo] ?? [NodeInfo]()
+        
+        roomWidth = NSUserDefaults.standardUserDefaults().floatForKey(RoomWidthKey)
+        if roomWidth <= 0.01 {
+            roomWidth = 5.0
+            NSUserDefaults.standardUserDefaults().setFloat(roomWidth, forKey: RoomWidthKey)
+        }
+        roomLength = NSUserDefaults.standardUserDefaults().floatForKey(RoomLengthKey)
+        if roomLength <= 0.01 {
+            roomLength = 10.0
+            NSUserDefaults.standardUserDefaults().setFloat(roomLength, forKey: RoomLengthKey)
+        }
+        
+        roomWidthTextField.text = NSString(format: "%.2f", roomWidth)
+         as String
+        roomLengthTextField.text = NSString(format: "%.2f", roomLength)
+            as String
         
         sceneSetup()
         
@@ -120,6 +150,34 @@ class HomeViewController: UIViewController {
         zoomInButton.enabled = true
     }
     
+    @IBAction func setRoomSizeButtonTapped(sender: UIButton) {
+        
+        let numberFormatter = NSNumberFormatter()
+        numberFormatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        roomWidth = (numberFormatter.numberFromString(roomWidthTextField.text!)?.floatValue)!
+        roomLength = (numberFormatter.numberFromString(roomLengthTextField.text!)?.floatValue)!
+        
+        if roomWidth <= 0.01 {
+            roomWidth = 5.0
+            roomWidthTextField.text = NSString(format: "%.2f", roomWidth)
+                as String
+            return
+        }
+
+        if roomLength <= 0.01 {
+            roomLength = 10.0
+            roomLengthTextField.text = NSString(format: "%.2f", roomLength)
+                as String
+            return
+        }
+        
+        NSUserDefaults.standardUserDefaults().setFloat(roomWidth, forKey: RoomWidthKey)
+        NSUserDefaults.standardUserDefaults().setFloat(roomLength, forKey: RoomLengthKey)
+        
+        setRoomStructure((CGFloat)(roomWidth), z: (CGFloat)(roomLength))
+        
+    }
+    
     func disableAllButtons() {
         translateButton.enabled = false
         rotateButton.enabled = false
@@ -140,45 +198,39 @@ class HomeViewController: UIViewController {
         // Constructing basic structure
         let leftWallGeometry = SCNBox(width: Constants.WallThickness, height: Constants.RoomYLength, length: z, chamferRadius: 0.0)
         leftWallGeometry.firstMaterial!.diffuse.contents = UIColor.lightGrayColor()
-        let leftWallNode = SCNNode(geometry: leftWallGeometry)
+        leftWallNode.geometry = leftWallGeometry
         leftWallNode.position = SCNVector3Make(-Float(x) / 2.0, Float(Constants.RoomYLength) / 2.0, 0)
-        staticGeometry.addChildNode(leftWallNode)
         
         let rightWallGeometry = SCNBox(width: Constants.WallThickness, height: Constants.RoomYLength, length: z, chamferRadius: 0.0)
         rightWallGeometry.firstMaterial!.diffuse.contents = UIColor.lightGrayColor()
-        let rightWallNode = SCNNode(geometry: rightWallGeometry)
+        rightWallNode.geometry = rightWallGeometry
         rightWallNode.position = SCNVector3Make(Float(x) / 2.0, Float(Constants.RoomYLength) / 2.0, 0)
-        staticGeometry.addChildNode(rightWallNode)
         
         let floorGeometry = SCNBox(width: x, height: Constants.WallThickness, length: z, chamferRadius: 0.0)
         floorGeometry.firstMaterial!.diffuse.contents = UIColor.lightGrayColor()
-        //let floorNode = SCNNode(geometry: floorGeometry)
         floorNode.geometry = floorGeometry
         floorNode.position = SCNVector3Make(0, -Float(Constants.WallThickness) / 2.0, 0)
-        staticGeometry.addChildNode(floorNode)
-        
-        //        let ceilingGeometry = SCNBox(width: Constants.RoomXLength, height: Constants.WallThickness, length: Constants.RoomZLength, chamferRadius: 0.0)
-        //        ceilingGeometry.firstMaterial!.diffuse.contents = UIColor.lightGrayColor()
-        //        let ceilingNode = SCNNode(geometry: ceilingGeometry)
-        //        ceilingNode.position = SCNVector3Make(0, Float(Constants.RoomYLength), 0)
-        //        staticGeometry.addChildNode(ceilingNode)
         
         let frontGeometry = SCNBox(width: x, height: Constants.RoomYLength, length: Constants.WallThickness, chamferRadius: 0.0)
         frontGeometry.firstMaterial!.diffuse.contents = UIColor.lightGrayColor()
-        let frontNode = SCNNode(geometry: frontGeometry)
+        frontNode.geometry = frontGeometry
         frontNode.position = SCNVector3Make(0, Float(Constants.RoomYLength) / 2.0, -Float(z) / 2.0)
-        staticGeometry.addChildNode(frontNode)
         
         let backGeometry = SCNBox(width: x, height: Constants.RoomYLength, length: Constants.WallThickness, chamferRadius: 0.0)
         backGeometry.firstMaterial!.diffuse.contents = UIColor.lightGrayColor()
-        let backNode = SCNNode(geometry: backGeometry)
+        backNode.geometry = backGeometry
         backNode.position = SCNVector3Make(0, Float(Constants.RoomYLength) / 2.0, Float(z) / 2.0)
-        staticGeometry.addChildNode(backNode)
+        
     }
     
     func sceneSetup () {
         
-        setRoomStructure(5, z: 10)
+        setRoomStructure((CGFloat)(roomWidth), z: (CGFloat)(roomLength))
+        staticGeometry.addChildNode(leftWallNode)
+        staticGeometry.addChildNode(rightWallNode)
+        staticGeometry.addChildNode(floorNode)
+        staticGeometry.addChildNode(frontNode)
+        staticGeometry.addChildNode(backNode)
         
         //debugLoadModelFromJSON()
         //debugLoadModelFromDae()
@@ -186,20 +238,6 @@ class HomeViewController: UIViewController {
         geometryNode.addChildNode(staticGeometry)
         geometryNode.addChildNode(dynamicGeometry)
         scene.rootNode.addChildNode(geometryNode)
-        
-//        let pointLightNode = SCNNode()
-//        pointLightNode.light = SCNLight()
-//        pointLightNode.light!.type = SCNLightTypeOmni
-//        pointLightNode.light!.color = UIColor(red: 1.0, green: 0.839, blue: 0.667, alpha: 1.0)
-//        pointLightNode.position = SCNVector3Make(0, Float(Constants.RoomYLength) / 2.0, -Float(Constants.RoomZLength) / 2.0 + 2.0 * Float(Constants.WallThickness))
-//        scene.rootNode.addChildNode(pointLightNode)
-//        
-//        let pointLightNode2 = SCNNode()
-//        pointLightNode2.light = SCNLight()
-//        pointLightNode2.light!.type = SCNLightTypeOmni
-//        pointLightNode2.light!.color = UIColor(red: 1.0, green: 0.945, blue: 0.878, alpha: 1.0)
-//        pointLightNode2.position = SCNVector3Make(Float(Constants.RoomXLength) / 2.0 - 2.0 * Float(Constants.WallThickness), Float(Constants.RoomYLength) / 2.0, Float(Constants.RoomZLength) / 2.0 - 2.0 * Float(Constants.WallThickness))
-//        scene.rootNode.addChildNode(pointLightNode2)
         
         camera.camera = SCNCamera()
         camera.camera!.usesOrthographicProjection = true
@@ -209,10 +247,6 @@ class HomeViewController: UIViewController {
         camera.position = SCNVector3Make(0, 0, 50)
         cameraOrbit.addChildNode(camera)
         scene.rootNode.addChildNode(cameraOrbit)
-        
-//        let targetNode = SCNLookAtConstraint(target: geometryNode);
-//        targetNode.gimbalLockEnabled = false;
-//        camera.constraints = [targetNode];
         
         for nodeInfo in myDesign {
             dynamicGeometry.addChildNode(nodeInfo.node)
@@ -228,15 +262,12 @@ class HomeViewController: UIViewController {
         
         sceneView.autoenablesDefaultLighting = true
         sceneView.scene = scene
-        //sceneView.allowsCameraControl = true
     }
     
     func handlePinch(sender: UIPinchGestureRecognizer) {
         switch state {
         case .Normal:
             break
-//            let z = camera.position.z + Float(sender.scale) / 2000.0 * Float(sender.velocity)
-//            camera.transform = SCNMatrix4Translate(camera.transform, camera.position.x, camera.position.y, z)
         case .Translate:
             selectedNode.position.y = selectedNode.position.y + Float(sender.scale) / 20.0 * Float(sender.velocity)
         default:
@@ -249,6 +280,7 @@ class HomeViewController: UIViewController {
         
         switch state {
         case .Normal:
+            
             // Rotate camera around global X axis
             var newCameraXRot = cameraXRot
             newCameraXRot -= (Float)(translation.y)*(Float)(M_PI)/180.0
@@ -268,22 +300,10 @@ class HomeViewController: UIViewController {
                 cameraXRot = newCameraXRot
                 geoZRot = newGeoZRot
             }
-            //geometryNode.eulerAngles.x -= (Float)(translation.y)*(Float)(M_PI)/180.0 / 3.0
-            //geometryNode.eulerAngles.y -= (Float)(translation.x)*(Float)(M_PI)/180.0 / 3.0
-//            var newCameraXRot = (Float)(translation.x)*(Float)(M_PI)/180.0
-//            newCameraXRot += cameraXRot
-//            
-//            camera.eulerAngles.y = newCameraXRot
-//            
-//            if(sender.state == UIGestureRecognizerState.Ended) {
-//                cameraXRot = newCameraXRot
-//            }
             
         case .Translate:
             let p = sender.locationInView(sceneView)
             let hitResults = sceneView.hitTest(p, options: [SCNHitTestRootNodeKey: staticGeometry])
-            
-            //var moveVector = SCNVector3Zero
             
             if hitResults.count > 0 {
                 
@@ -291,23 +311,12 @@ class HomeViewController: UIViewController {
                 let node = result.node
                 
                 if node == floorNode {
+                    
                     let hitLocation = result.worldCoordinates
-//                    moveVector = SCNVector3Make(hitLocation.x - selectedNode.position.x, 0, hitLocation.z - selectedNode.position.z)
                     selectedNode.position = SCNVector3Make(hitLocation.x, selectedNode.position.y, hitLocation.z)
                 }
             }
             
-//            if(sender.state == UIGestureRecognizerState.Ended) {
-//                selectedNode.position = SCNVector3Make(selectedNode.position.x + moveVector.x, 0, selectedNode.position.z + moveVector.z)
-//            }
-            
-            //break
-            
-//            let deltaX = (Float)(translation.x) / 1000.0
-//            let deltaZ = (Float)(translation.y) / 1000.0
-//            selectedNode.position = SCNVector3Make(selectedNode.position.x + deltaX, selectedNode.position.y, selectedNode.position.z + deltaZ)
-            
-//            selectedNode.transform = SCNMatrix4MakeTranslation(xCoord, selectedNode.position.y, zCoord)
         case .Rotate:
             let deltaAngle = (Float)(translation.x)*(Float)(M_PI)/180.0 / 3.0
             selectedNode.eulerAngles.y = selectedNode.eulerAngles.y + deltaAngle
@@ -316,6 +325,7 @@ class HomeViewController: UIViewController {
     }
     
     func handleTap(gestureRecognize: UIGestureRecognizer) {
+        self.view.endEditing(true)
         
         switch state {
         case .Normal:
@@ -329,27 +339,12 @@ class HomeViewController: UIViewController {
                 }
                 
                 if node.parentNode == dynamicGeometry {
-                    //node.geometry?.firstMaterial?.diffuse.contents = UIColor.darkGrayColor()
-                    //TransformWidget(node: node)
                     state = .Translate
                     selectNode(node)
                 }
             }
             
         case .Translate:
-//            let p = gestureRecognize.locationInView(sceneView)
-//            let hitResults = sceneView.hitTest(p, options: [SCNHitTestRootNodeKey: staticGeometry])
-//
-//            if hitResults.count > 0 {
-//                let result: SCNHitTestResult = hitResults[0]
-//                let node = result.node
-//
-//                if node == floorNode {
-//                    let hitLocation = result.worldCoordinates
-//                    selectedNode.position = SCNVector3Make(hitLocation.x, selectedNode.position.y, hitLocation.z)
-//                }
-//            }
-
             break
         case .Rotate:
             break
@@ -369,8 +364,6 @@ class HomeViewController: UIViewController {
             return SCNNode()
         }
     
-//        let faces = data["faces"] as? [AnyObject]
-//        let intFaces = (faces as! [Int]).map({Int32($0)})
         let indexData = NSData(bytes: indices, length: sizeof(Int)*indices.count)
     
         let source = SCNGeometrySource(data: positionData, semantic:
@@ -380,27 +373,12 @@ class HomeViewController: UIViewController {
 
         let modelGeometry = SCNGeometry(sources: [source], elements: [element])
         return SCNNode(geometry: modelGeometry)
-    
-//        let boxGeo = SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0.1)
-//        boxGeo.firstMaterial!.diffuse.contents = UIColor.lightGrayColor()
-//        return SCNNode(geometry: boxGeo)
-
     }
     
     func debugLoadModelFromJSON() {
         let location = NSString(string:"/Users/shapeare/Documents/misc_chair01.js").stringByExpandingTildeInPath
         let rawData = NSData(contentsOfFile: location)
         let modelData = try? NSJSONSerialization.JSONObjectWithData(rawData!, options: NSJSONReadingOptions.AllowFragments)
-        
-//        let dict = modelData as! NSDictionary
-//        guard let vertices = dict["vertices"] as? [Float32] else {
-//            print("error casting vertices")
-//            return
-//        }
-//        guard let indices = dict["faces"] as? [Int] else {
-//            print("error casting indices")
-//            return
-//        }
         
         let modelNode = modelFromJson(modelData as! NSDictionary)
         
